6aef589e337c0d2dab0f2e1a8dcd349f97b790bb
h_kuramoto <h.kuramoto@bravetechnology.co.jp>
2023-12-01 16:50:25 +0900

    OGP 取得

--- liff-google-alert/src/pages/index.vue
+++ liff-google-alert/src/pages/index.vue
@@ -9,7 +9,49 @@ v-card(
 </template>
 
 <script setup lang="ts">
+import { JSDOM } from "jsdom";
 const config = useRuntimeConfig()
+
+// OGP 情報取得
+const getUrlOgp = (url: string) => {
+  fetch(url).then(res => res.text()).then(text => {
+    const jsdom = new JSDOM();
+    const parser = new jsdom.window.DOMParser();
+    const el = parser.parseFromString(text, "text/html")
+    const headEls = (el.head.children)
+    let imageUrl
+    let description
+    Array.from(headEls).map(v => {
+      const prop = v.getAttribute('property')
+      console.log(prop, v.getAttribute("content"))
+      switch(prop) {
+        case 'og:image':
+          imageUrl = v.getAttribute("content")
+          break;
+        case 'og:description':
+          description = v.getAttribute("content")
+          break;
+        default:
+          break;
+      }
+    })
+  })
+}
+
+// URL の整形
+const formatURL = (url: string) => {
+  const searchString = 'url'
+  var regex = new RegExp(url);
+  var match = regex.exec(searchString);
+
+  console.log(match)
+  if (match) {
+    var result = url.substring(match.index + match[0].length);
+    console.log(result);
+  } else {
+    console.log("特定の文字列は見つかりませんでした。");
+  }
+}
 
 // notionDB 一覧取得
 const notion = useNotion();
@@ -17,7 +59,11 @@ const response = await notion.databases.query({ database_id: config.public.notio
 
 // 取得データをループ
 const alertLists = response.results.map((item, index) => {
-  const alert = { title: item.properties.Title.title[0].plain_text, url:  item.properties.URL.url, publishedDate: item.properties.公開日.date.start }
+  const formattedUrl = formatURL(item.properties.URL.url)
+  // OGP 情報取得
+  getUrlOgp(formattedUrl)
+
+  const alert = { title: item.properties.Title.title[0].plain_text, url: formattedUrl, publishedDate: item.properties.公開日.date.start }
   return alert
 })
 
